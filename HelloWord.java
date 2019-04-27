e6542042d29fc6dd8bf5d8dca3d9710173dca6fc
pragyagupta1792 <pragyagupta1792@gmail.com>
2019-04-27 20:48:57 +0530

    first commit

---
+++ HelloWord.java
@@ -0,0 +1,13 @@
+public class HelloWord{
+
+   public static void main(String arg[]){
+
+            for(int i=1;i<=10;i++){
+ 
+            System.out.println("Hello");
+
+                }
+
+    }
+
+}
